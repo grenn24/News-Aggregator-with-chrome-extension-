1378385b83573f40938a9260fefcac3a262734c6
grenn24 <126308058+grenn24@users.noreply.github.com>
2023-03-27 23:33:55 +0800

    updated footer_news.js

--- news/search/static/footer_news.js
+++ news/search/static/footer_news.js
@@ -1,1 +1,30 @@
+// Get the footer element
+const footer = document.querySelector('footer');
 
+// Define variable for previous scroll position as existing vertical distance scrolled from the top window
+var prevScrollpos = window.pageYOffset;
+
+// Activate the below script when user scrolls up or down
+window.addEventListener('scroll', () => {
+  
+// Define variable for updated scrolling position
+var currentScrollPos = window.pageYOffset;
+  
+// If user is scrolling up
+if (prevScrollpos > currentScrollPos) {
+    
+    // Add the 'invisible' class to the footer
+    footer.classList.remove('visible');
+    footer.classList.add('invisible');
+  }  
+  
+// If user is scrolling down
+else {
+    
+    // Add the 'visible' class from the footer
+    footer.classList.remove('invisible');
+    footer.classList.add('visible');
+  }
+
+// Update previous scroll position to current scroll position for the next cycle
+prevScrollpos = currentScrollPos;
