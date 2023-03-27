4ed68a07d5ab8cf4e3236045b6af0a4c25b4e3f2
grenn24 <126308058+grenn24@users.noreply.github.com>
2023-03-27 23:50:59 +0800

    updated main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 # Define Endpoint and API keys for retrieving news articles
 NEWS_API_ENDPOINT = 'https://newsapi.org/v2/top-headlines'
+NEWS_API_ENDPOINT2 = 'https://newsapi.org/v2/everything'
 api_key1 = '9e58ca6414334896bd1793b25bf49103'
 api_key2 = 'f54b2d435bd547228c270a373172061f'
 
