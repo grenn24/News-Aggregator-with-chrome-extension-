ab91b64ac9414a07421a3336db93d2ee9ab0927b
grenn24 <126308058+grenn24@users.noreply.github.com>
2023-03-27 15:07:55 +0800

    added home route in main.py

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ import requests
 
 app = Flask(__name__)
 
-#Define API keys for retrieving news articles
+#Define Endpoint and API keys for retrieving news articles
 NEWS_API_ENDPOINT = 'https://newsapi.org/v2/top-headlines'
 api_key1 = '9e58ca6414334896bd1793b25bf49103'
 api_key2 = 'f54b2d435bd547228c270a373172061f'
+
+#Define home route
+@app.route('/')
+def home():
+    #Define list of different news categories
+    categories = ['business', 'technology', 'sports', 'entertainment']
+    
+    news_columns = []
+    latest_news = []
