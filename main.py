1a1247067882f1c8492118fa4a16031b7ed0b582
grenn24 <126308058+grenn24@users.noreply.github.com>
2023-03-27 15:17:38 +0800

    added javascript styling route in main.py

--- main.py
+++ main.py
@@ -3,31 +3,31 @@ import requests
 
 app = Flask(__name__)
 
-#Define Endpoint and API keys for retrieving news articles
+# Define Endpoint and API keys for retrieving news articles
 NEWS_API_ENDPOINT = 'https://newsapi.org/v2/top-headlines'
 api_key1 = '9e58ca6414334896bd1793b25bf49103'
 api_key2 = 'f54b2d435bd547228c270a373172061f'
 
-#Define home route
+# Define home route
 @app.route('/')
 def home():
-    #Define list of different news categories
+    # Define list of different news categories
     categories = ['business', 'technology', 'sports', 'entertainment']
     
     news_columns = []
     latest_news = []
     
-    #Retrieve news articles in the 'general' category
+    # Retrieve news articles in the 'general' category
     params = {'category': 'general', 'country': 'us', 'apiKey': api_key2, 'language': 'en'}
     response = requests.get(NEWS_API_ENDPOINT, params=params)
     latest_articles = response.json().get('articles', [])[:2]
     
-    #Add them into the latest_news list
+    # Add them into the latest_news list
     latest_news.append({
         'latests': latest_articles
     }}
     
-    #Retrieve 3 articles from the other categories respectively and add them into the news_column list    
+    # Retrieve 3 articles from the other categories respectively and add them into the news_column list    
     for category in categories:
         params = {'category': category, 'apiKey': api_key2, 'language': 'en'}
         response = requests.get(NEWS_API_ENDPOINT, params=params)
@@ -42,10 +42,15 @@ def home():
 # Define route for a specific news category        
 @app.route('/news/<topic>')
 def news(topic):
-    #Retrieve articles in the specified news category        
+    # Retrieve articles in the specified news category        
     params = {'category': topic, 'apiKey': api_key2, 'language': 'en'}
     response = requests.get(NEWS_API_ENDPOINT, params=params)
     articles = response.json().get('articles')
     
-    #Render the html template with the articles     
+    # Render the html template with the articles     
     return render_template('news.html', articles=articles, topic=topic.capitalize())
+
+# Define route to access javascript styling file for footer        
+@app.route('/news/static/footer_news.js')
+def news_static_footer_news():
+    return app.send_static_file('footer_news.js')
