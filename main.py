8bfcb3d776b14e736780d7771133f045fdf4a5c6
grenn24 <126308058+grenn24@users.noreply.github.com>
2023-03-27 15:05:04 +0800

    added news api keys in main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
+
+#Define API keys for retrieving news articles
+NEWS_API_ENDPOINT = 'https://newsapi.org/v2/top-headlines'
+api_key1 = '9e58ca6414334896bd1793b25bf49103'
+api_key2 = 'f54b2d435bd547228c270a373172061f'
